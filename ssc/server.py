ee334feca68c0e4b48c510dbf18489bc7ac7f5d3
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-18 21:43:14 +0000

    Invite user is done

--- ssc/server.py
+++ ssc/server.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, jsonify, request, abort
 
-from ssc.Invites.invites import getInvitesForUser, processInviteResponse
+from ssc.Invites.invites import fetch_user_invites, process_invite, insert_user_invite
 
 app = Flask(__name__, template_folder='testflask/templates')
 
@@ -13,23 +13,37 @@ def homeDummy():
 
 
 @app.route("/api/invites/<username>", methods=["GET"])
-def inviteUser(username):
-    listOfInvites = getInvitesForUser(username)
-    res={'invites': listOfInvites}
+def get_user_invites(username):
+    list_of_invites = fetch_user_invites(username)
+    res = {'invites': list_of_invites}
     return jsonify(res);
 
+
 @app.route("/api/invites/<username>", methods=["POST"])
-def updateInvite(username):
+def update_invite(username):
     print(username)
     print(request.json)
     if (not request.json) | ('accept' not in request.json) | ('workspace' not in request.json):
         abort(400)
 
-    res = processInviteResponse(username, request.json)
+    res = process_invite(username, request.json)
     return jsonify({'invitesProcessed': res});
+
+
+@app.route("/api/invites", methods=["POST"])
+def invite_user():
+    print(request.json)
+    if (not request.json) | ('username' not in request.json) \
+            | ('workspace' not in request.json) | ('invitedBy' not in request.json):
+        abort(400)
+
+    res = insert_user_invite(request.json)
+    res_json = {'user_invited': res}
+    if (res==False): res_json['error'] = 'Could not invite user. Check user is admin or invite still exits'
+    return jsonify(res_json);
 
 
 if __name__ == "__main__":
     # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run( port=port)
+    app.run(port=port)
