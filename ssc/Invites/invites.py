0d3f4a58c624cb410d2ddb55939df46ea2686104
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-18 18:21:57 +0000

    accept or reject invite done

--- ssc/Invites/invites.py
+++ ssc/Invites/invites.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+#TODO reuse methods everywhere
+
 def getInvitesForUser(username):
     try:
         connection = psycopg2.connect(
@@ -39,3 +41,64 @@ def getInvitesForUser(username):
     return listOfInvites;
 
 
+def processInviteResponse(username, inviteResponse):
+    decision = inviteResponse['accept']
+    workspace = inviteResponse['workspace']
+
+    try:
+        connection = psycopg2.connect(
+            database="ssc")
+        cursor = connection.cursor()
+
+        getInviteId_pgQuery = """select i.invite_id, i.user_id, i.invited_by_id, i.workspace_id
+                                    from invites i, users u, workspaces w 
+                                    where u.username=%s and w.name=%s
+                                    and u.user_id=i.user_id and w.workspace_id=i.workspace_id"""
+
+        cursor.execute(getInviteId_pgQuery, (username,workspace))
+        invites = cursor.fetchone()
+        if (invites == None):
+            return []
+
+        inviteId=invites[0]
+        userId=invites[1]
+        invitedBy = invites[2]
+        workspaceId = invites[3]
+
+
+        print(inviteId, userId, workspaceId, invitedBy)
+
+        if (decision == True):
+            print('true')
+            insertUserWorkspace_pgQuery = """insert into workspace_users (user_id, workspace_id, is_admin)
+                                        values(%s, %s, false)"""
+            cursor.execute(insertUserWorkspace_pgQuery, (userId, workspaceId))
+            connection.commit()
+            count = cursor.rowcount
+
+            if (count==0):
+                return 0;
+
+            deleteInvite_pgQuery = "delete from invites where invite_id=%s"
+            cursor.execute(deleteInvite_pgQuery, (inviteId,))
+            connection.commit()
+            count = cursor.rowcount
+
+        else:
+            print('false')
+            deleteInvite_pgQuery = "delete from invites where invite_id=%s"
+            cursor.execute(deleteInvite_pgQuery, (inviteId,))
+            connection.commit()
+            count = cursor.rowcount
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    return count
--- ssc/server.py
+++ ssc/server.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
-from ssc.Invites.invites import getInvitesForUser
+from ssc.Invites.invites import getInvitesForUser, processInviteResponse
 
 app = Flask(__name__, template_folder='testflask/templates')
 
@@ -15,14 +15,21 @@ def homeDummy():
 @app.route("/api/invites/<username>", methods=["GET"])
 def inviteUser(username):
     listOfInvites = getInvitesForUser(username)
+    #TODO should return with a key called invites
     return jsonify(listOfInvites);
 
-@app.route("/users")
-def usersDummy():
-    return "Hello, Users"
+@app.route("/api/invites/<username>", methods=["POST"])
+def updateInvite(username):
+    print(username)
+    print(request.json)
+    if (not request.json) | ('accept' not in request.json) | ('workspace' not in request.json):
+        abort(400)
+
+    res = processInviteResponse(username, request.json)
+    return jsonify({'invitesProcessed': res});
 
 
 if __name__ == "__main__":
     # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run( port=port)
