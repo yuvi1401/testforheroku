53fbac28c14434a2ca0d7f894fb50c5a92199515
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-21 18:38:50 +0000

    all error handling done

--- ssc/Workspaces/workspaces.py
+++ ssc/Workspaces/workspaces.py
@@ -7,6 +7,9 @@ from ssc.dbconfig import user, password, database
 
 
 def delete_workspace(delete_request):
+    connection=None
+    workspace_deleted = False
+    res={}
     deleted_by = delete_request['deleted_by']
     workspace = delete_request['workspace']
     loop = asyncio.new_event_loop()
@@ -15,30 +18,32 @@ def delete_workspace(delete_request):
     loop = asyncio.new_event_loop()
     deleted_by_id = loop.run_until_complete(get_user_id(deleted_by))
 
-    if (workspace_id == -1 | deleted_by_id == -1):
-        return False
-
     try:
-        connection = psycopg2.connect(
-            user=user,
-            password=password,
-            database=database)
-        cursor = connection.cursor()
+        if (workspace_id == -1 | deleted_by_id == -1):
+            res['error'] = 'Could not locate workspace or user deleting the workspace'
+        else:
+            connection = psycopg2.connect(
+                user=user,
+                password=password,
+                database=database)
+            cursor = connection.cursor()
 
-        loop = asyncio.new_event_loop()
-        admin_status = loop.run_until_complete(is_user_admin(deleted_by_id, workspace_id))
+            loop = asyncio.new_event_loop()
+            admin_status = loop.run_until_complete(is_user_admin(deleted_by_id, workspace_id))
 
-        if (admin_status == 0):
-            return False
-
-        delete_workspace_sql = "delete from workspaces where workspace_id=%s"
-        cursor.execute(delete_workspace_sql, (workspace_id,))
-        connection.commit()
-        count = cursor.rowcount
+            if (admin_status == 0):
+                res['error'] = 'User is not an admin of the workspace'
+            else:
+                delete_workspace_sql = "delete from workspaces where workspace_id=%s"
+                cursor.execute(delete_workspace_sql, (workspace_id,))
+                connection.commit()
+                count = cursor.rowcount
+                if (count!=0):
+                    workspace_deleted = True
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
-        return False
+        res['error']=error
 
     finally:
         # closing database connection.
@@ -47,11 +52,15 @@ def delete_workspace(delete_request):
             connection.close()
             print("PostgreSQL connection is closed")
 
-    if (count == 0): return False
-    return True
+        res['workspace_deleted'] = workspace_deleted
+        return res
 
 
 def update_admin(workspace, admin_request):
+    workspace_admin_updated = False
+    connection = None
+    res= {}
+
     username = admin_request['username']
     admin_username = admin_request['admin_username']
     make_admin = admin_request['make_admin']
@@ -65,37 +74,41 @@ def update_admin(workspace, admin_request):
     loop = asyncio.new_event_loop()
     admin_id = loop.run_until_complete(get_user_id(admin_username))
 
-    if (workspace_id == -1 | admin_id == -1 | user_id == -1):
-        return False
-
     try:
-        connection = psycopg2.connect(
-            user=user,
-            password=password,
-            database=database)
-        cursor = connection.cursor()
 
-
-        loop = asyncio.new_event_loop()
-        admin_status = loop.run_until_complete(is_user_admin(admin_id, workspace_id))
-
-        if (admin_status == 0):
-            return False
-
-        if (make_admin == 'True'):
-            make_admin_bool = True;
+        if (workspace_id == -1 | admin_id == -1 | user_id == -1):
+            res['error'] = 'Invalid input. Check username, admin and workspace are correct'
         else:
-            make_admin_bool = False;
+            connection = psycopg2.connect(
+                user=user,
+                password=password,
+                database=database)
+            cursor = connection.cursor()
 
-        update_admin_sql = "update workspace_users set is_admin=%s where workspace_id=%s" \
-                           "and user_id=%s"
-        cursor.execute(update_admin_sql, (make_admin_bool, workspace_id, user_id))
-        connection.commit()
-        count = cursor.rowcount
+            loop = asyncio.new_event_loop()
+            admin_status = loop.run_until_complete(is_user_admin(admin_id, workspace_id))
+
+            if (admin_status == 0):
+                res['error'] = 'Admin is not actual admin of workspace'
+            else:
+                if (make_admin == 'True'):
+                    make_admin_bool = True;
+                else:
+                    make_admin_bool = False;
+
+                update_admin_sql = "update workspace_users set is_admin=%s where workspace_id=%s" \
+                                   "and user_id=%s"
+                cursor.execute(update_admin_sql, (make_admin_bool, workspace_id, user_id))
+                connection.commit()
+                count = cursor.rowcount
+                if (count == 0):
+                    res['error'] = 'Could not make user as admin'
+                else:
+                    workspace_admin_updated = True
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
-        return False
+        res['error'] = str(error)
 
     finally:
         # closing database connection.
@@ -104,40 +117,49 @@ def update_admin(workspace, admin_request):
             connection.close()
             print("PostgreSQL connection is closed")
 
-    if (count == 0): return False
-    return True
+
+        res['workspace_admin_updated'] = workspace_admin_updated
+        return res
 
 
 def create_workspace_only(data):
+    res={}
+    workspace_added = False
+    connection=None
     try:
         workspace_name = data['name']
         admin = data['admin'];
         loop = asyncio.new_event_loop()
         admin_id = loop.run_until_complete(get_user_id(admin))
 
+        if(admin_id==-1):
+            res['error'] = 'Could not find user in the system so cannot add workspace for user'
+        else:
+            connection = psycopg2.connect(
+                user=user,
+                password=password,
+                database=database)
 
-        connection = psycopg2.connect(
-            user=user,
-            password=password,
-            database=database)
+            insert_workspace_name = "insert into workspaces (name) values (%s) returning workspace_id"
 
-        insert_workspace_name = "insert into workspaces (name) values (%s) returning workspace_id"
+            cursor = connection.cursor()
+            cursor.execute(insert_workspace_name, (workspace_name,))
 
-        cursor = connection.cursor()
-        cursor.execute(insert_workspace_name, (workspace_name,))
-
-        connection.commit()
-        count = cursor.rowcount
-        if (count == 0):
-            return False;
-
-        new_workspace_id = cursor.fetchone()[0]
-
-        add_user_to_workspace([admin_id], new_workspace_id, True)
+            connection.commit()
+            count = cursor.rowcount
+            if (count == 0):
+                res['error'] = 'Could not add workspace into the system'
+            else:
+                new_workspace_id = cursor.fetchone()[0]
+                admin_added = add_user_to_workspace([admin_id], new_workspace_id, True)
+                if(admin_added!=0):
+                    workspace_added=True
+                else:
+                    res['error'] = 'Workspace created but could not set admin. Contact support'
 
     except (Exception, psycopg2.Error) as error:
         print('Error while conecting to PostgresQL', error)
-        return False
+        res['error'] = error
     finally:
 
         if (connection):
@@ -145,14 +167,17 @@ def create_workspace_only(data):
             cursor.close()
             connection.close()
             print("PostgresSQL connection is closed")
-
-    return True
+        res['workspace_added']=workspace_added
+        return res
 
 
 def create_workspace_with_users(data):
     users = data['users'];
     admin = data['admin'];
     workspace = data['name'];
+    res={}
+    users_added=False
+    connection=None
 
     try:
         connection = psycopg2.connect(
@@ -167,25 +192,28 @@ def create_workspace_with_users(data):
 
         count = cursor.rowcount
         if (count == 0):
-            return count;
-
-        new_workspace_id = cursor.fetchone()[0]
-        loop = asyncio.new_event_loop()
-        admin_id = loop.run_until_complete(get_user_id(admin))
-        admin_added = add_user_to_workspace([admin_id], new_workspace_id, True);
-        if (admin_added == 0):
-            return admin_added;
-
-        user_id_list = []
-        for user in users:
+            res['error'] = 'Could not create the workspace'
+        else:
+            new_workspace_id = cursor.fetchone()[0]
             loop = asyncio.new_event_loop()
-            single_user_id = loop.run_until_complete(get_user_id(user['username']))
-            user_id_list.append(single_user_id)
-
-        users_added = add_user_to_workspace(user_id_list, new_workspace_id);
+            admin_id = loop.run_until_complete(get_user_id(admin))
+            admin_added = add_user_to_workspace([admin_id], new_workspace_id, True);
+            if (admin_added == 0):
+                res['error'] = 'Workspace added but could not set admin to workspace.'
+            else:
+                user_id_list = []
+                for user in users:
+                    loop = asyncio.new_event_loop()
+                    single_user_id = loop.run_until_complete(get_user_id(user['username']))
+                    user_id_list.append(single_user_id)
+                users_added = add_user_to_workspace(user_id_list, new_workspace_id);
+                if(users_added!=len(users)):
+                    res['error'] = 'Some users could not be added to workspace. Try again'
+                else:
+                    users_added=True
     except (Exception, psycopg2.Error) as error:
         print('Error while conecting to PostgresQL', error)
-        return 0
+        res['error'] = str(error)
     finally:
         if (connection):
             # close the connection and the cursor
@@ -193,7 +221,8 @@ def create_workspace_with_users(data):
             connection.close()
             print("PostgresSQL connection is closed")
 
-    return users_added;
+        res['users_added'] = users_added
+        return res
 
 
 def add_user_to_workspace(list_of_ids, workspace_id, is_admin=False):
@@ -295,41 +324,43 @@ def delete_user_from_workspace(data):
 
 
 def fetch_workspace_files(name):
+    list_of_files=[]
+    res={}
+    connection=None
     try:
         connection = psycopg2.connect(
             database="ssc")
 
-        print("Using Python variable in Pg Query")
-
         cursor = connection.cursor()
         if (name == None):
-            return []
+            res["error"] = "Workspace name is invalid"
+        else:
+            loop = asyncio.new_event_loop()
+            workspace_id = loop.run_until_complete(get_workspace_id(name))
+            if (workspace_id == -1):
+                res["error"] = "Workspace name is invalid"
+            else:
+                cursor.execute("""SELECT file_name FROM workspace_files
+                       INNER JOIN workspaces ON workspaces.workspace_id = workspace_files.workspace_id
+                       WHERE workspaces.workspace_id = %s
+                       """, (workspace_id,))
 
-        loop = asyncio.new_event_loop()
-        workspace_id = loop.run_until_complete(get_workspace_id(name))
-        if (workspace_id == -1):
-            return []
+                workspace_files = cursor.fetchall()
 
-
-        cursor.execute("""SELECT file_name FROM workspace_files
-               INNER JOIN workspaces ON workspaces.workspace_id = workspace_files.workspace_id
-               WHERE workspaces.workspace_id = %s
-               """, (workspace_id,))
-
-        workspace_files = cursor.fetchall()
-
-        list_of_files = []
-        for row in workspace_files:
-            list_of_files.append(
-                {'file_name': row[0]})
+                for row in workspace_files:
+                    list_of_files.append(
+                        {'file_name': row[0]})
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
+        res["error"] = error
     finally:
         # closing database connection.
         if (connection):
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
-
-    return list_of_files;
+        if ((len(list_of_files) == 0) & ("error" not in res)):
+            res["error"] = "There are no files in this workspace"
+        res["files"] = list_of_files
+        return res
--- ssc/server.py
+++ ssc/server.py
@@ -12,32 +12,12 @@ from ssc.Users.users import fetch_users, add_user, fetch_user_workspaces
 
 from ssc.login.get_logged_in import fetch_user_details
 
-from werkzeug.utils import secure_filename
-
 app = Flask(__name__, template_folder='testflask/templates')
+
 
 @app.route("/")
 def homeDummy():
     return 'Hello'
-
-
-@app.route("/api/audio", methods=['POST'])
-def handle_audio():
-    # target = os.path.join(UPLOAD_FOLDER, 'test_docs')
-    # if not os.path.isdir(target):
-    #     os.mkdir(target)
-    print('In handle audio')
-    file = request.files['file']
-    print(file)
-    # print(open(file, 'rb'))
-    print(file.filename)
-    filename = secure_filename(file.filename)
-
-    # destination = "/".join([target, filename])
-    # file.save(destination)
-    # session['uploadFilePath'] = destination
-    response = "Whatever you wish too return"
-    return jsonify(response)
 
 
 @app.route('/api/login', methods=['GET'])
@@ -98,8 +78,7 @@ def delete_user():
     if ("error" in res):
         return res_json, 404
     else:
-        return res_json, 200
-
+        return res_json, 204
 
 
 @app.route("/api/invites", methods=["POST"])
@@ -113,7 +92,7 @@ def invite_user():
     if ("error" in res):
         return res_json, 404
     else:
-        return res_json, 200
+        return res_json, 201
 
 
 @app.route("/api/invites/<username>", methods=["GET"])
@@ -125,6 +104,7 @@ def get_user_invites(username):
     else:
         return res_json, 200
 
+
 @app.route("/api/invites/<username>", methods=["POST"])
 def update_invite(username):
     if (not request.json) | ('accept' not in request.json) | ('workspace' not in request.json):
@@ -135,10 +115,8 @@ def update_invite(username):
     if ("error" in res):
         return res_json, 404
     else:
-        return res_json, 200
+        return res_json, 201
 
-
-# TODO
 
 @app.route('/api/workspaces', methods=['POST'])
 def handle_create_workspace():
@@ -147,22 +125,14 @@ def handle_create_workspace():
     else:
         if ('users' in request.json):
             res = create_workspace_with_users(request.json)
-            if (res == len(request.json['users'])):
-                res_json = {'workspace_added': True}
-            elif (res != 0):
-                res_json = {'workspace_added': False,
-                            'error': 'Workspace added but not all users added.'}
-            else:
-                res_json = {'workspace_added': False,
-                            'error': 'Workspace could not be added.'}
         else:
             res = create_workspace_only(request.json)
-            if (res):
-                res_json = {'workspace_added': True}
-            else:
-                res_json = {'workspace_added': False,
-                            'error': 'Could not add workspace or set admin.'}
 
+        res_json = jsonify(res);
+        if ("error" in res):
+            return res_json, 404
+        else:
+            return res_json, 201
         return jsonify(res_json);
 
 
@@ -172,17 +142,21 @@ def handle_delete_workspace():
         abort(400)
 
     res = delete_workspace(request.json)
-    res_json = {'workspace_deleted': res}
-    if (res == False): res_json['error'] = 'Could not delete workspace. ' \
-                                           'Check user is admin or workspace still exists'
-    return jsonify(res_json);
+    res_json = jsonify(res)
+    if ("error" in res):
+        return res_json, 404
+    else:
+        return res_json, 204
 
 
 @app.route("/api/workspaces/<name>", methods=["GET"])
 def get_workspace_file(name):
-    list_of_files = fetch_workspace_files(name)
-    res = {'files': list_of_files}
-    return jsonify(res);
+    res = fetch_workspace_files(name)
+    res_json = jsonify(res);
+    if ("error" in res):
+        return res_json, 404
+    else:
+        return res_json, 200
 
 
 @app.route("/api/workspaces/<workspace_name>", methods=["PUT"])
@@ -192,12 +166,11 @@ def handle_update_workspace(workspace_name):
         abort(400)
 
     res = update_admin(workspace_name, request.json)
-    print(res)
-    res_json = {'workspace_admin_updated': res}
-    if (res == False): res_json['error'] = 'Could not update workspace. ' \
-                                           'Check admin user is an admin'
-
-    return jsonify(res_json);
+    res_json = jsonify(res);
+    if ("error" in res):
+        return res_json, 404
+    else:
+        return res_json, 201
 
 
 if __name__ == "__main__":
